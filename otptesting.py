9903cf6f65986ab1d944972d62b01a40df185bf2
PHolmesOnGit <106204236+PHolmesOnGit@users.noreply.github.com>
2024-05-02 19:32:14 -0600

    Update otptesting.py
    Updated info to match set up instructions

--- otptesting.py
+++ otptesting.py
@@ -4,7 +4,7 @@ testingkey = pyotp.random_base32()
 
 print(testingkey)
 
-provisioning_uri = pyotp.totp.TOTP(testingkey).provisioning_uri(name='codingwithholmes@google.com', issuer_name='1Pass But Worse')
+provisioning_uri = pyotp.totp.TOTP(testingkey).provisioning_uri(name='email or whatever you want', issuer_name='name of password manager')
 
 print(provisioning_uri)
 
